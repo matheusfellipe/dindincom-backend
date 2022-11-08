88d4d27942e57fdb9cecf2bfc5db21f1a257ed10
Matheus Fellipe <matheus060399@gmail.com>
2022-11-08 00:25:12 -0300

    my first commit

--- src/controllers/cremosinho.js
+++ src/controllers/cremosinho.js
@@ -4,60 +4,75 @@ const conexao = require('../database/conexao');
 module.exports = {
     async index(requisicao, resposta) {
 
-    
+
         // const [count] = await conexao.raw('SELECT COUNT(*) cremosinho');
         // console.log(count);
         const cremosinho = await conexao.raw(`
         SELECT * FROM cremosinho ORDER BY sabor
         `)
-            
 
-        
+
+
         return resposta.json(cremosinho);
     },
     async criar(requisicao, resposta) {
-        const { sabor,vlr_unitario,qtd_estoque,inativo='f' } = requisicao.body;
-       
 
-        const res = await conexao.raw(`INSERT INTO cremosinho (sabor,vlr_unitario,qtd_estoque,inativo) VALUES (?,?,?,?)`,[
-            sabor,
-            vlr_unitario,
-            qtd_estoque,
-            inativo
-        ])
-        return resposta.send(res);
+        try {
+            const { sabor, vlr_unitario, qtd_estoque, inativo = 'f' } = requisicao.body;
+
+
+            const res = await conexao.raw(`INSERT INTO cremosinho (sabor,vlr_unitario,qtd_estoque,inativo) VALUES (?,?,?,?)`, [
+                sabor,
+                vlr_unitario,
+                qtd_estoque,
+                inativo
+            ])
+            return resposta.send(res);
+
+        } catch (error) {
+            return resposta.status(400).json({ message: "Não foi possivel criar" })
+        }
+
     },
 
     async delete(requisicao, resposta) {
-        const { id } = requisicao.params;
-        
+        try {
+            const { id } = requisicao.params;
+            await conexao.raw('DELETE FROM cremosinho WHERE id_cremosinho = ?', [id])
+            return resposta.status(204).send(id);
 
-        
-        await conexao.raw('DELETE FROM cremosinho WHERE id_cremosinho = ?',[id])
-        return resposta.status(204).send(id);
+        } catch (error) {
+            return resposta.status(400).send(error);
+        }
+
     },
 
     async update(requisicao, resposta) {
-        const { id } = requisicao.params;
-        const { sabor,vlr_unitario,qtd_estoque,inativo} = requisicao.body;
+        try {
+            const { id } = requisicao.params;
+            const { sabor, vlr_unitario, qtd_estoque, inativo } = requisicao.body;
 
-        
-        await conexao.raw('UPDATE cremosinho SET sabor = ?,vlr_unitario= ?,qtd_estoque = ?, inativo = ?',[
-            sabor,vlr_unitario,qtd_estoque,inativo
-        ])
-        return resposta.status(204).send(id);
+
+            await conexao.raw('UPDATE cremosinho SET sabor = ?,vlr_unitario= ?,qtd_estoque = ?, inativo = ? WHERE id_cremosinho = ?', [
+                sabor, vlr_unitario, qtd_estoque, inativo, id
+            ])
+
+        } catch (error) {
+            return resposta.status(204).send(error);
+        }
+
     },
     async consultarUm(requisicao, resposta) {
         try {
             const { id } = requisicao.query;
             const cremosinho = await conexao.raw(`
-            SELECT * FROM cremosinho WHERE id_cremosinho = ?`,[id]
+            SELECT * FROM cremosinho WHERE id_cremosinho = ?`, [id]
             )
-            
+
             return resposta.json(cremosinho);
         } catch (error) {
-            console.log(error)
+            return resposta.status(400).send(error)
         }
-      
+
     }
 }
