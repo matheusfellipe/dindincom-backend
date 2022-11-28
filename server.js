e0168b42c6760967de0901148aa2f94dd3663bd1
Matheus Fellipe <matheus060399@gmail.com>
2022-11-28 02:31:53 -0300

    my first commit

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3333;
 const app = express();
-const usuarioRota = require('./src/routes/usuario')
-const cremosinhoRota = require('./src/routes/cremosinho')
+const usuarioRota = require('./src/routes/usuario');
+const cremosinhoRota = require('./src/routes/cremosinho');
+const entregadorRota = require('./src/routes/entregador');
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true,
 }))
-app.use(usuarioRota)
-app.use(cremosinhoRota)
+app.use(usuarioRota);
+app.use(cremosinhoRota);
+app.use(entregadorRota);
 
 
 app.listen(port,()=>{
---
+++ src/controllers/entregador.js
@@ -0,0 +1,75 @@
+const conexao = require('../database/conexao');
+
+
+module.exports = {
+    async index(requisicao, resposta) {
+
+        const entregador = await conexao.raw(`
+        SELECT * FROM entregador ORDER BY nome
+        `)
+
+        return resposta.json(entregador.rows);
+    },
+    async criar(requisicao, resposta) {
+
+        try {
+            const { nome, cpf, telefone, placa_veiculo,rota} = requisicao.body;
+
+
+            const res = await conexao.raw(`INSERT INTO entregador (nome,cpf,telefone,placa_veiculo,rota) VALUES (?,?,?,?,?)`, [
+                nome,
+                cpf,
+                telefone,
+                placa_veiculo,
+                rota
+            ])
+            console.log(res.rows)
+            return resposta.send(res.rows);
+
+        } catch (error) {
+            return resposta.status(400).json({ message: "Não foi possivel criar" })
+        }
+
+    },
+
+    async delete(requisicao, resposta) {
+        try {
+            const { id } = requisicao.params;
+            await conexao.raw('DELETE FROM entregador WHERE id_entregador = ?', [id])
+            return resposta.status(204).send(id);
+
+        } catch (error) {
+            return resposta.status(400).send(error);
+        }
+
+    },
+
+    async update(requisicao, resposta) {
+        try {
+            const { id } = requisicao.params;
+            const { nome, cpf, telefone, placa_veiculo,rota } = requisicao.body;
+
+
+            await conexao.raw('UPDATE entregador SET sabor = ?,vlr_unitario= ?,qtd_estoque = ?, inativo = ? WHERE id_entregador = ?', [
+                nome, cpf, telefone, placa_veiculo,rota, id
+            ])
+
+        } catch (error) {
+            return resposta.status(204).send(error);
+        }
+
+    },
+    async consultarUm(requisicao, resposta) {
+        try {
+            const { id } = requisicao.query;
+            const entregador = await conexao.raw(`
+            SELECT * FROM entregador WHERE id_entregador = ?`, [id]
+            )
+
+            return resposta.json(entregador);
+        } catch (error) {
+            return resposta.status(400).send(error)
+        }
+
+    }
+}
